02325deb92e5b5e0bbab8ddc63237408f0490ffe
yelab07 <79489573+yelab07@users.noreply.github.com>
2022-04-14 23:28:54 -0500

    updated delete,updatetext,completed functionality

--- server-with-mdb/server/routes/api-routes.js
+++ server-with-mdb/server/routes/api-routes.js
@@ -1,18 +1,66 @@
 const router = require("express").Router();
 const Todo = require("../models/todoModel");
+const { findByIdAndDelete } = require("../models/todoModel");
+
+// const db = require("../models");
 
 // get all the todos from mongodb using mongoose
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const myTodos = await Todo.find({});
+    res.send(myTodos);
+  } catch (error) {
+    res.json({ msg: err });
+  }
+});
 
 router.post("/", async (req, res) => {
-  const newTodo = new Todo({ text: req.body.text, completed: false });
-  res.json(await newTodo.save());
+  try {
+    const newTodo = new Todo({ text: req.body.text, completed: false });
+    res.json(await newTodo.save());
+  } catch (err) {
+    res.json({ msg: err });
+  }
 });
 
 // will update a Todo with mongoose. findByIdAndUpdate
-router.put("/update", (req, res) => {});
+router.put("/updatetext", async (req, res) => {
+  try {
+    const updateResponse = await Todo.findByIdAndUpdate(req.body.id, {
+      text: req.body.text,
+    });
+
+    if (updateResponse !== null) return res.json({ msg: "success" });
+    res.json({ msg: "wrong id sent" });
+  } catch (err) {
+    res.json({ msg: err });
+  }
+});
+// will update a Todos completed status in mongoose. findByIdAndUpdate
+router.put("/updatecompleted", async (req, res) => {
+  try {
+    const updateResponse = await Todo.findByIdAndUpdate(req.body.id, {
+      completed: req.body.completed,
+    });
+    // console.log(this.updateResponse);
+    if (updateResponse !== null) return res.json({ msg: "success" });
+    res.json({ msg: "wrong id sent" });
+  } catch (err) {
+    res.json({ msg: err });
+  }
+});
 
 // will delete a todo. findByIdAndDelete
-router.put("/delete", (req, res) => {});
+
+router.put("/delete", async (req, res) => {
+  try {
+    const deleteResposne = await Todo.findByIdAndDelete(req.body.id);
+
+    if (deleteResposne !== null) return res.json({ msg: "success" });
+    res.json({ msg: "wrong id sent" });
+  } catch (err) {
+    res.json({ msg: err });
+  }
+});
 
 module.exports = router;
